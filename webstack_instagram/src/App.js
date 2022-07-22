5db296d710dcfe2b0ecc3322ec6e77baa0778c08
einsteinnnamah <einsteinnnamah@gmail.com>
2022-07-22 18:10:53 +0100

    structure

---
+++ webstack_instagram/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <form>
+      <span><input placeholder='+234' value='' /></span>
+      <span><input placeholder='Enter your number' /></span>
+     </form>
+    </div>
+  );
+}
+
+export default App;
