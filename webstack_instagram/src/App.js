cf1eee860d165b4daf99f83ddd0a458b7cf7989f
einsteinnnamah <einsteinnnamah@gmail.com>
2022-07-23 19:31:30 +0100

    updates

--- webstack_instagram/src/App.js
+++ webstack_instagram/src/App.js
@@ -1,13 +1,10 @@
-
 import './App.css';
-import Button from './Button';
 const App = () => {
   
   const name = 'Webstack Challange';
   return (
     <div className="App">
       <h1>Hello this is the {name}</h1>
-      <Button alertText="Welcome to a new future" />
       <p></p>
     </div>
 
