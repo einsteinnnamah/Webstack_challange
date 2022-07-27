6551cfdcd4635c68462822315514492f2d20a888
einsteinnnamah <einsteinnnamah@gmail.com>
2022-07-27 21:15:30 +0100

    bio_in_progress

--- webstack_instagram/src/Components/Bio.jsx
+++ webstack_instagram/src/Components/Bio.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import profileIcon from "../assets/profileIcon.svg"
 
 const Bio = () => {
+  const Bio_stats = {
+    followers: '1.5M',
+    following: '1.5M',
+    posts: '1.5M'
+  }
+
+  const editForm = () => {
+    <form className='edit-bio-form'>
+        <input type="text" name="" id="" placeholder='Enter your Username' />
+        <div className=''>
+         <label htmlFor="">Bio</label>
+          <input type="text" name="" id="" placeholder='Fill in your Bio details' />
+        </div>
+     
+    </form>
+  }
+
+
   return (
-    <div>Bio</div>
+    <div className='Bio_wrapper'>
+      <div className='Profile_intro'>
+        <div className='Profile_image'>
+          <img src={profileIcon} alt="Einstein Pics" />
+        </div>
+        <div className='Profile_text'>
+          <div className='Profile_text_header'>
+            <h1>Chibuezennamah</h1>
+            <div className='Profile_text_header_btn'>
+              <button className='edit_btn'>Edit Profile</button>
+            </div>
+          
+          </div>
+          <div className='Bio_stats'>
+            <p><strong>{Bio_stats.posts}</strong>  posts </p>
+            <p><strong>{Bio_stats.followers}</strong> followers </p>
+            <p><strong>{Bio_stats.following}</strong> following </p>
+          </div>
+          <div>
+            <p>Chibueze Nnamah</p>
+            <p className='Content'>Digital Creator</p>
+            <p>Front end Developer & Interface Designer</p>
+            <p>wwwe</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
